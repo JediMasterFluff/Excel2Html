e3a8256e4e63924e76529bf6fad860fff3563dad
JediMasterFluff <fluffyman35@gmail.com>
2017-03-28 09:22:24 -0700

    Push to git

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+/*
+ * This project is to be used my the members of the DKA to take their spreadsheet data
+ * and create customizable HTML tables that they can copy/paste into our web site CMS
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
