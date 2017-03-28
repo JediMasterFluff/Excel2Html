571707f992f339de3174a872a9fd43527fb68a64
JediMasterFluff <fluffyman35@gmail.com>
2017-03-28 10:58:41 -0700

    Adding Screen

---
+++ src/forms/MainScreen.java
@@ -0,0 +1,11 @@
+package forms;
+
+import javax.swing.SwingConstants;
+
+public class MainScreen implements SwingConstants {
+
+	public MainScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
